2e3e3093e16608cef7c74802b1abc3bf02aa36bd
TaLongPhi <91329809+TaLongPhi@users.noreply.github.com>
2021-11-19 21:39:10 +0700

    STM32-LCD

---
+++ BT_ThucHnah_STM32-LCD/STM32LCD/Core/Src/main.c
@@ -0,0 +1,197 @@
+#include "main.h"
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+//HAM GUI LENH
+ void send8bit(char D)
+ {
+	 int b0,b1,b2,b3,b4,b5,b6,b7;
+	 if ((D &1)==0) b0=0;		//2^0
+	 else b0=1;
+	 if ((D &2)==0) b1=0;
+	 else b1=1;
+	 if ((D &4)==0) b2=0;
+	 else b2=1;
+	 if ((D &8)==0) b3=0;
+	 else b3=1;
+	 if ((D &16)==0) b4=0;
+	 else b4=1;
+	 if ((D &32)==0) b5=0;
+	 else b5=1;
+	 if ((D &64)==0) b6=0;
+	 else b6=1;
+	 if ((D &128)==0) b7=0;
+	 else b7=1;
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_0,b0);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_1,b1);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_2,b2);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_3,b3);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_4,b4);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_5,b5);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_6,b6);
+	 HAL_GPIO_WritePin(GPIOD,GPIO_PIN_7,b7);
+ }
+void sendlcd(char cmd)
+{
+	// Rw dat muc thap
+	//b2 dat chan rs=0,de noi rang cmd la lenh
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_RESET);
+	//B3 GUI 8 BIET VAO 8 PIN
+	send8bit(cmd);
+	//b4 enable cho kytu -- < lcd
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+	HAL_Delay(10);
+} 
+// ham gui ky tu
+void sendchar(char kytu)
+{
+	// Rw dat muc thap
+	//b2 dat chan rs=1
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3, GPIO_PIN_SET);
+	//B3 GUI 8 BIET VAO 8 PIN
+	send8bit(kytu);
+	//b4 enable cho kytu -- < lcd
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_4, GPIO_PIN_SET);
+	HAL_Delay(10);
+}
+void sendstring(char *str)
+{
+	for (int i =0;str[i] !='\0';i++)
+	sendchar(str[i]);
+}
+int main(void)
+{
+  
+  HAL_Init();
+  SystemClock_Config();
+  MX_GPIO_Init();
+	//xoa noi dung
+	sendlcd(0x01);
+	// bat hien thi man hinh, tat con tro
+	sendlcd(0x0C);
+	//hello
+	sendstring("Hello K61 NTU");
+	//xuong dong
+	sendlcd(0x38);//che do 2 dong
+	sendlcd(0xC0);
+	sendstring("Ta Long Phi");
+
+  while (1)
+  {
+    
+  }
+  
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE2);
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOD, GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_4, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PD0 PD1 PD2 PD3
+                           PD4 PD5 PD6 PD7 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3
+                          |GPIO_PIN_4|GPIO_PIN_5|GPIO_PIN_6|GPIO_PIN_7;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PB3 PB4 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_4;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     tex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
